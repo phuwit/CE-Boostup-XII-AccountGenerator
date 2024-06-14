42a960099827a86aee25d700f3dec2f0f1fcb40c
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2024-06-14 23:54:11 +0700

    done

--- main.ts
+++ main.ts
@@ -1,2 +1,100 @@
 import fs from "fs";
-import { parse } from "csv-parse";
+import { parse } from "csv";
+import { generate } from "generate-password-browser";
+import { error } from "console";
+
+type StudentRecord = {
+  rowNumber: string;
+  ID: string;
+  sector: string;
+  prefix_th: string;
+  firstName_th: string;
+  lastName_th: string;
+  fullName_th: string;
+  nickName_th: string;
+  firstName_en: string;
+  lastName_en: string;
+  fullName_en: string;
+  nickName_en: string;
+  email_personal: string;
+  email_university: string;
+  phoneNumber: string;
+  instagram: string;
+  lineID: string;
+  discordUsernameFromUser: string;
+  discordID: string;
+  positions: string;
+};
+
+type Role = "Admin" | "User" | "Staff" | "Reviewer" | "Admin" | "SuperAdmin";
+
+interface CreateUserDto {
+  displayName: string;
+  email: string;
+  roles: Role[];
+  group?: string;
+  password?: string;
+}
+
+const processFile = async () => {
+  const records: StudentRecord[] = [];
+  const parser = fs.createReadStream("./student-list.csv").pipe(
+    parse({
+      columns: true,
+    })
+  );
+  for await (const record of parser) {
+    records.push(record);
+  }
+  return records;
+};
+
+function generatePassword(length: number) {
+  const charset =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*";
+}
+
+(async () => {
+  const records = await processFile();
+
+  const createUserDtos: CreateUserDto[] = records.map((record) => {
+    const password = generate({
+      length: 16,
+      numbers: true,
+      symbols: true,
+      lowercase: true,
+      uppercase: true,
+      strict: true,
+      exclude: '()+_-=}{[]|:;"/?.><,`~',
+    });
+    const createUserDto: CreateUserDto = {
+      displayName: record.nickName_en,
+      email: record.email_university,
+      roles: ["Staff"],
+      password: password,
+    };
+    return createUserDto;
+  });
+
+  const credentials = createUserDtos.map((dto) => {
+    return `${dto.email}:${dto.password}\n`;
+  });
+  fs.writeFile("credentials.txt", credentials.join(""), (err) => {
+    if (err) throw err;
+  });
+
+  createUserDtos.forEach(async (dto) => {
+    console.log(dto);
+    fetch("http://localhost:3000/grader/api/users", {
+      method: "POST",
+      body: JSON.stringify(dto),
+      headers: {
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxNzYxMzdmNi1iMDdhLTQ1NDEtYWJjYS00YWZhNDc2NDI0OTEiLCJyb2xlcyI6WyJTdXBlckFkbWluIl0sImlhdCI6MTcxODM4MzYwOSwiZXhwIjoxNzE4OTg4NDA5fQ.uRTVShUPTT3678aVNasmqNZhOPO2BnkFZyIsq0cKaB8",
+        "Content-Type": "application/json"
+      }
+    })
+      .then(async (response) => console.log(await response.json()))
+      .catch((error) => console.error(error));
+  });
+  // console.info(records);
+})();
